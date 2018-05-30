fb66e5306ec1fc7b52a5f002c600f6216ac3bc57
Danny <seungwon@treenod.com>
2018-05-30 13:57:32 +0900

    First Step

---
+++ Assets/Script/Pathfinding/TargetNodes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetNodes : MonoBehaviour 
+{
+	private Transform			_transform;
+	private PolygonCollider2D	_polygonCollider2D;
+
+	private List<Pathfinding.GridNode>	_nodes	= new List<Pathfinding.GridNode>();
+
+	void Awake()
+	{
+		_transform			= this.transform;
+		_polygonCollider2D	= GetComponent<PolygonCollider2D>();
+	}
+
+	void Start()
+	{
+		// collider bound내에 외곽 노드 추출
+		if (_polygonCollider2D == null)
+		{
+			return;
+		}
+
+		var nodesInRegion = AstarPath.active.data.gridGraph.GetNodesInRegion(_polygonCollider2D.bounds);
+
+		for (int nodeIndex = 0; nodeIndex < nodesInRegion.Count; ++nodeIndex)
+		{
+			Pathfinding.GridNode gridNode = (Pathfinding.GridNode)nodesInRegion[nodeIndex];			
+			if (gridNode.Walkable == false)
+			{
+				_nodes.Add(gridNode);
+			}
+		}
+	}
+
+	public List<Pathfinding.GridNode> GetNodes()
+	{
+		return _nodes;
+	}
+}
---
+++ Assets/Script/Spawner/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour 
+{
+	public float		radius;
+	public float		delay;
+	public int 			remainSpawnCount;
+	public float		interval;
+	public int			spawnCount;
+
+	public GameObject	enemyPrefab;
+	public Transform	enemyGroupTransform;
+	
+	private int			_spawnedCount	= 0;
+	private Transform	_transform;
+
+	void Awake()
+	{
+		_transform	= this.transform;
+	}
+	
+	void Start()
+	{
+		StartCoroutine(spawnCoroutine());
+	}
+
+	IEnumerator	spawnCoroutine()
+	{
+		if (delay > 0)
+		{
+			yield return new WaitForSeconds(delay);
+		}
+
+		float elapsedTime	= interval;
+		while (remainSpawnCount < 0 || remainSpawnCount > _spawnedCount)
+		{
+			if (elapsedTime <= 0)
+			{
+				for (int spawnedIndex = 0; spawnedIndex < spawnCount; ++spawnedIndex)
+				{
+					Vector2		spawnPosition	= new Vector2(_transform.position.x, _transform.position.y) + Random.insideUnitCircle * radius;
+					GameObject	enemyObject		= Instantiate(enemyPrefab, spawnPosition, Quaternion.identity, enemyGroupTransform);
+
+					++_spawnedCount;
+
+					if (_spawnedCount >= remainSpawnCount)
+					{
+						yield break;
+					}
+				}
+
+				elapsedTime = interval;
+			}
+			else
+			{
+				elapsedTime -= Time.deltaTime;
+			}
+
+			yield return null;
+		}
+	}
+}
