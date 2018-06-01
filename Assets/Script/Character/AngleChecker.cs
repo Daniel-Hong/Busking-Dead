a564036c796594b6ea82370417184093b561389e
Danny <seungwon@treenod.com>
2018-06-01 09:48:24 +0900

    added Range Checker

---
+++ Assets/Script/Character/AngleChecker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleChecker : MonoBehaviour 
+{
+	public Vector3		forwardVector;
+	public float		angle;
+	public float		distance;
+
+	Transform	_transform;
+
+	void Awake()
+	{
+		_transform	= this.transform;
+	}
+
+	public bool WithinRange(Vector3 position)
+	{
+		Vector3 delta	= position - _transform.position;
+		if (delta.magnitude	> distance)
+		{
+			Debug.Log("Out of distance");
+
+			return false;
+		}
+
+		if (Vector3.Dot(forwardVector, delta.normalized) < Mathf.Cos(angle * Mathf.Deg2Rad * 0.5f))
+		{
+			Debug.Log("Out of angle");
+
+			return false;
+		}
+
+		Debug.Log("Within Range");
+
+		return true;
+	}
+}
